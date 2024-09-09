1dca7c1dd88fd5cf48d8a305893334f057176d01
Esteban Rivas <34036824+RAES9@users.noreply.github.com>
2024-09-09 08:52:28 -0600

    Update discord_auth.py

--- auth/discord_auth.py
+++ auth/discord_auth.py
@@ -2,7 +2,7 @@ import requests
 
 client_id = '1233485159571918898'
 client_secret = 'CeOYp_SWOMvXSHDOfHozhtrqI0g1Eq8a'
-redirect_uri = "https://kpis-tribal-ios.streamlit.app"
+redirect_uri = "https://kpistribalworldwide.streamlit.app"
 
 
 def get_access_token(code):
